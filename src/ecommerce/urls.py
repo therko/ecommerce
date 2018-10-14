d7e40587b09a46defe6efa4b6234c02a75997e8d
therko <pderko@gmail.com>
2018-10-14 19:05:40 +0200

    s3e27

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view
 
 from .views import home_page, about_page, contact_page, login_page, register_page
 
@@ -28,6 +29,10 @@ urlpatterns = [
     path('contact/', contact_page),
     path('login/', login_page),
     path('register/', register_page),
+    path('products/', ProductListView.as_view()),
+    path('products-fbv/', product_list_view),
+    path('products/<int:pk>', ProductDetailView.as_view()),
+    path('products-fbv/<int:pk>', product_detail_view),
     path('admin/', admin.site.urls),
 ]
 
